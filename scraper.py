8c3f7da626efbdc9d67cebc5fdd35ed3f2ac3821
sicvermilion <sicvermilion@users.noreply.github.com>
2014-08-24 22:59:35 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,9 +14,9 @@ class AirbnbScraper:
         self.cookies = {}
         self.debug = debug
 
-    def listing_url(self, offset):
-        return ('https://m.airbnb.com/api/-/v1/listings/search?location=semarang&number_of_guests=1&offset=%s&guests=1&items_per_page=20'
-                % (offset))
+    def listing_url(self, price_min,price_max,offset):
+        return ('https://m.airbnb.com/api/-/v1/listings/search?location=bali&price_min=%s&price_max=%s&number_of_guests=1&offset=%s&guests=1&items_per_page=20'
+                % (price_min,price_max,offset))
 
     def get(self, url, referer='', min_sleep=30, max_add=120, xhr=False):
         if self.debug:
@@ -35,51 +35,61 @@ class AirbnbScraper:
         return r
 
     def crawl(self):
-        offset = 0
-        count = 999
-        while offset < count:
-            r = self.get(self.listing_url(offset), referer='https://m.airbnb.com/s/semarang')
-            list_arr = []
+        price_min = 0
+        while price_min < 401:
+            if price_min == 400:
+                price_max = 9999
+            else:
+                price_max = price_min + 24
 
-            try:
-                js = json.loads(r.content)
-                count = js['listings_count']
+            offset = 0
+            count = 999
+            while offset < count:
+                r = self.get(self.listing_url(price_min,price_max,offset), referer='https://m.airbnb.com/s/bali')
+                list_arr = []
 
-                if len(js['listings']) == 0:
-                    break
-                else:
-                    for listing1 in js['listings']:
-                        listing = listing1['listing']
-                        list_off = {
-                            "id": listing['id'],
-                            "city": listing['city'],
-                            "picture_url": listing['picture_url'],
-                            "user_id": listing['user_id'],
-                            "price": listing['price'],
-                            "price_native": listing['price_native'],
-                            "lat": listing['lat'],
-                            "lng": listing['lng'],
-                            "name": listing['name'],
-                            "address": listing['address'],
-                            "property_type": listing['property_type'],
-                            "room_type_category": listing['room_type_category'],
-                            "smart_location": listing['smart_location'],
-                            "reviews_count": listing['reviews_count'],
-                            "user_name": listing['user']['user']['first_name'],
-                            "url": "https://www.airbnb.com/rooms/" + str(listing['id']),
-                            "date_added": time.strftime('%Y-%m-%d %H:%M:%S')
-                        }
-                        list_arr.append(list_off)
-                        
-                offset += 20
-                scraperwiki.sqlite.save(["id"],list_arr)
-                if self.debug:
-                    print "new offset", offset
+                try:
+                    js = json.loads(r.content)
+                    count = js['listings_count']
 
-            except ValueError as e:
-                print >> sys.stderr, 'received ValueError:', e
-                print >> sys.stderr, 'error: could not parse response'
-                sys.exit(1)
+                    if len(js['listings']) == 0:
+                        break
+                    else:
+                        for listing1 in js['listings']:
+                            listing = listing1['listing']
+                            list_off = {
+                                "id": listing['id'],
+                                "city": listing['city'],
+                                "picture_url": listing['picture_url'],
+                                "user_id": listing['user_id'],
+                                "price": listing['price'],
+                                "price_native": listing['price_native'],
+                                "lat": listing['lat'],
+                                "lng": listing['lng'],
+                                "name": listing['name'],
+                                "address": listing['address'],
+                                "property_type": listing['property_type'],
+                                "room_type_category": listing['room_type_category'],
+                                "smart_location": listing['smart_location'],
+                                "reviews_count": listing['reviews_count'],
+                                "user_name": listing['user']['user']['first_name'],
+                                "bedsroom": listing['bedsroom'],
+                                "url": "https://www.airbnb.com/rooms/" + str(listing['id']),
+                                "date_added": time.strftime('%Y-%m-%d %H:%M:%S')
+                            }
+                            list_arr.append(list_off)
+                            
+                    offset += 20
+                    scraperwiki.sqlite.save(["id"],list_arr)
+                    if self.debug:
+                        print "new offset", offset
+
+                except ValueError as e:
+                    print >> sys.stderr, 'received ValueError:', e
+                    print >> sys.stderr, 'error: could not parse response'
+                    sys.exit(1)
+
+            price_min += 25
 
 if __name__ == "__main__":
     ab = AirbnbScraper()
